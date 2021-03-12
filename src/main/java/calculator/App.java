8e90ba1916d675e04b9bfcc472987f99219164fc
VaishnaviDhulipalla2902 <nagasri.vaishnavi@iiitb.org>
2021-03-12 16:32:13 +0530

    Updated Code

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -3,69 +3,102 @@ import java.util.*;
 public class App {
 
 	public static void main(String[] args){
-		Scanner scan = new Scanner(System.in);
-
+		Scanner reader = new Scanner(System.in);
+		int op, flag=0;
+		double num1, num2;
 		System.out.println("------------Calculator--------------");
+		do{
+			System.out.println("Choices of Operations:");
+            System.out.println("");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Exit");
+			System.out.println("");
+            System.out.print("Enter your choice(number): ");
+			System.out.println("");
+			op = reader.nextInt();
+			if(op==5) flag = 1;
+			else{				
+				switch(op){
+					
+					case 1:  // Addition
+						System.out.println("You choose Addition!!");
+						System.out.println("");
+						System.out.print("Enter number 1: ");
+						num1 = reader.nextDouble();
+						System.out.print("Enter number 2: ");
+						num2 = reader.nextDouble();
+						add(num1,num2);
+					break;
 
+					case 2: //Subtraction
+						System.out.println("You choose Subtraction!!");
+						System.out.println("");
+						System.out.print("Enter number 1: ");
+						num1 = reader.nextDouble();
+						System.out.print("Enter number 2: ");
+						num2 = reader.nextDouble();
+						sub(num1,num2);
+					break;
 
-		while(true){
-      int num1,num2;
-      System.out.println("Enter two numbers");
-      num1 = scan.nextInt();
-      num2 = scan.nextInt();
-			System.out.println("Enter 1 to add");
-			System.out.println("Enter 2 to subtract");
-			System.out.println("Enter 3 to multiply");
-			System.out.println("Enter 4 to divide");
+					case 3: //Multiplication
+						System.out.println("You choose Multiplication!!");				
+						System.out.println("");
+						System.out.print("Enter number 1: ");
+						num1 = reader.nextDouble();
+						System.out.print("Enter number 2: ");
+						num2 = reader.nextDouble();
+						mul(num1,num2);
+					break;
 
-			System.out.println("Enter 5 to exit");
+					case 4: //Division
+						System.out.println("You choose Division!!");
+						System.out.println("");
+						System.out.print("Enter number 1: ");
+						num1 = reader.nextDouble();
+						System.out.print("Enter number 2: ");
+						num2 = reader.nextDouble();
+						div(num1,num2);
+					break;
 
-			System.out.println();
-			System.out.println("Enter your choice: ");
-			int choice = scan.nextInt();
-			switch(choice){
-			case 1:
-			add(num1,num2);
-			break;
-			case 2:
-			sub(num1,num2);
-			break;
-			case 3:
-			mul(num1,num2);
-			break;
-			case 4:
-			div(num1,num2);
-			break;
-			case 9: System.out.println("Exiting calculator....");
-			System.exit(0);
-			break;
-			default: System.out.println("Exiting due to invalid input!!");
-			}
+					default: 
+						System.out.println("Exiting due to invalid input!!");
+						flag = 1;
+					}
+				}		
+		    }while(flag == 0);
 		}
 
+	public static double add(double num1,double num2){
+		double c = num1+num2;
+		System.out.println("");
+		System.out.println("The Result is "+c);
+		System.out.println("");
+		return c;
 	}
 
-	public static int add(int num1,int num2){
-		int sum = num1+num2;
-		System.out.println("Result is "+sum);
-		return sum;
+	public static double sub(double num1,double num2){
+	    double c = num1-num2;
+		System.out.println("");
+		System.out.println("The Result is "+(num1-num2));
+		System.out.println("");
+		return c;
 	}
-
-	public static int sub(int num1,int num2){
-	      int c = num1-num2;
-		  System.out.println("Result is "+(num1-num2));
-		  return c;
-	  }
-	public static int mul(int num1,int num2){
-		int c = num1*num2;	
-		  System.out.println("Result is "+ (num1*num2));
-		  return c;
-		}
-
-	  public static int div(int num1,int num2){
-		    int c = num1/num2;
-			  System.out.println("Result is "+ (num1/num2));
-			  return c;
-		  }
+	public static double mul(double num1,double num2){
+		double c = num1*num2;	
+		System.out.println("");
+		System.out.println("The Result is "+ (num1*num2));
+		System.out.println("");
+		return c;
+	}
+	  public static double div(double num1,double num2){
+		double c = num1/num2;
+		System.out.println("");
+		System.out.println("The Result is "+ (num1/num2));
+		System.out.println("");
+		return c;
+	}
 
 }
