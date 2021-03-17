cbbf4d71f04c938b9fc3b77cf0f48ed9ab0e50b5
VaishnaviDhulipalla2902 <nagasri.vaishnavi@iiitb.org>
2021-03-17 15:41:14 +0530

    Updated Code

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,4 +1,4 @@
-package calculator;
+// package calculator;
 import java.util.*; 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
--- src/test/java/calculator/AppTest.java
+++ src/test/java/calculator/AppTest.java
@@ -1,6 +1,4 @@
-package calculator;
-
-
+// package calculator;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
