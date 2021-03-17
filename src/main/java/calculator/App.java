ba2fc32d2ef92c0fa1fcc2c4987c42fbb84c7d6c
VaishnaviDhulipalla2902 <nagasri.vaishnavi@iiitb.org>
2021-03-17 14:53:38 +0530

    Updated pom.xml

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,5 +1,7 @@
 package calculator;
 import java.util.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 public class App {
 	private static final Logger logger = LogManager.getLogger(App.class);
 	public static void main(String[] args){
